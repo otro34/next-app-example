013f8e3c4825fd538272dc47e9503a7ec733c9fa
Juan Romaina Acevedo <Juan.Romaina@tektonlabs.com>
2023-05-09 16:17:00 -0500

    initial commit

---
+++ src/app/about/page.js
@@ -0,0 +1,15 @@
+import Header from '../../components/Header/Header'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <Header />
+            <h2>
+            ACERCA DE NOSOTROS
+            </h2>
+        </div>
+
+    )
+} 
+
+export default AboutUs

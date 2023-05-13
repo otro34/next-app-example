7da5eca5509ee0e69c5d4aa59d48ab6c6fe776d2
Juan Carlos Romaina <otro34@hotmail.com>
2023-05-12 22:13:36 -0500

    adding bootstrap

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import Header from '../../components/Header/Header'
 
 const AboutUs = () => {
@@ -7,6 +9,7 @@ const AboutUs = () => {
             <h2>
             ACERCA DE NOSOTROS
             </h2>
+            <button type="button" className="btn btn-primary">Primary</button>
         </div>
 
     )

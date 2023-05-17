fb60c3c6f13959ef8552bf3ea532261ad24b1e3c
Juan Carlos Romaina <otro34@hotmail.com>
2023-05-16 20:34:36 -0500

    adding empty sol

--- src/app/ejercicio4/page.js
+++ src/app/ejercicio4/page.js
@@ -1,16 +1,6 @@
 'use client';
 
-import { useState } from 'react'
-import Form from 'react-bootstrap/Form'
-import './styles.css'
-
-import Chip from '../../components/Chip/Chip.jsx'
-
 const Ejercicio3 = () => {
-
-    const frutas = ["Manzana", "Pera", "Naranja", "Mandarina", "Pomelo"]
-    const [arr, setArr] = useState(frutas)
-    const [textBusqueda, setTextBusqueda] = useState("")
 
     return (
         <div>
@@ -21,19 +11,6 @@ const Ejercicio3 = () => {
                 Crear un componente compartido. El componente deberá renderizar un chip. 
                 Usar el chip para renderizar el contenido de la lista.   
             </p>
-            <Form.Control 
-                type="text"
-                value={textBusqueda}
-                onChange={e => setTextBusqueda(e.target.value)}
-                ></Form.Control>
-            <ul className="nobullets">
-                    { arr.filter(f => f.includes(textBusqueda))
-                        .map(fruta =>{
-                        return (<li key={fruta} >
-                            <Chip text={fruta} />
-                            </li>)
-                    }) } 
-            </ul>
         </div>
     )
 } 
--- src/app/ejercicio5/page.js
+++ src/app/ejercicio5/page.js
@@ -1,28 +1,9 @@
 'use client';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
-import { useState } from 'react'
 
 const Ejercicio5 = () => {
-    const [usuario, setUsuario] = useState('')
-    const [password, setPassword] = useState('')
-
-    const handleClick = () => {
-        alert(usuario + " " + password)
-
-    }
-
-    const handleOnUserChange = (e) => {
-        setUsuario(e.target.value);
-    }
-
-    const handleOnPasswordChange = (e) => {
-        setPassword(e.target.value);
-    }
 
     return (
-        <><div>
+        <div>
             <h2>
                 Ejercicio 5
             </h2>
@@ -31,18 +12,7 @@ const Ejercicio5 = () => {
                 se debe comparar usuario y password contra valores en una lista.
                 Si es exitoso, se debe redirigir hacia el ejercicio 6.
             </p>
-        </div><div>
-                <Form.Control 
-                    type="text" 
-                    placeholder="Usuario"
-                    onChange={handleOnUserChange} />
-                <br />
-                <Form.Control 
-                    type="password" 
-                    placeholder="Password" 
-                    onChange={handleOnPasswordChange} />
-                <Button onClick={handleClick}>INGRESAR</Button>
-            </div></>
+        </div>
     )
 } 
 

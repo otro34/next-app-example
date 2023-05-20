42f55051775c766fd2f6324b7dab492e1088b6e6
Juan Carlos Romaina <otro34@hotmail.com>
2023-05-20 10:32:59 -0500

    modificando ejemplo

--- src/app/ejercicio8/page.js
+++ src/app/ejercicio8/page.js
@@ -10,7 +10,9 @@ const Ejercicio8 = () => {
                 En base al ejercicio 7. crear una tabla que liste personas.
                 cargar la lista de personas desde un archivo separado (javascript) que contenga un arreglo de 
                 objetos (persona). el listado debe tener un enlace que dirija a la página con el detalle de la 
-                persona. La información de la persona se debe pasar a ese componente.
+                persona. La información de la persona se debe pasar a ese componente. 
+                Construya un formulario para guardar los datos de la persona. 
+                
             </p>
         </div>
     )
